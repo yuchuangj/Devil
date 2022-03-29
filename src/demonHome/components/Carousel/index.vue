0ad8968539eb7f3344f737107857bce737c1e4a3
yuchuangj <1141687525@qq.com>
2022-03-29 17:59:59 +0800

    修改了轮播界面

---
+++ src/demonHome/components/Carousel/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <view class="Carousel-box">
+    <!--Swiper轮播-->
+    <swiper
+      class="swiper"
+      circular="true"
+      autoplay="true"
+      interval="5000"
+      indicator-dots="true"
+      indicator-active-color="rgba(143, 134, 86, 0.5)"
+    >
+      <swiper-item><img src="../Carousel/images/1.jpg" /></swiper-item>
+      <swiper-item><img src="../Carousel/images/2.jpg" /></swiper-item>
+    </swiper>
+  </view>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {},
+})
+export default class MonsterCaursel extends Vue {
+  public CarouselImg() {
+    uni.request({
+      url: "http://localhost:81/MonsterCarouselinfo/Carousel.php",
+      method: "GET",
+      dataType: "json",
+      success: (res) => {
+        console.log(res);
+      },
+      fail: (res) => {
+        console.error(res);
+      },
+    });
+  }
+  onLoad() {
+    this.CarouselImg();
+  }
+}
+</script>
+<style lang="scss">
+@import "./index.scss";
+</style>
--- src/demonHome/index.vue
+++ src/demonHome/index.vue
@@ -1,20 +1,24 @@
 <template>
     <view class="indexBox">
         <monster-caursel></monster-caursel>
+        <recommended-today></recommended-today>
     </view>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import MonsterCaursel from './components/Caursel/index.vue';
+import MonsterCaursel from './components/Carousel/index.vue';
+import RecommendedToday from './components/RecommendedToday/index.vue'
+
 @Component({
     components:{
         MonsterCaursel, //轮播组件
+        RecommendedToday, //今日推荐
     }
 })
 export default class demonHome extends Vue{
     public title="完美"
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 @import "./index.scss"
 </style>
